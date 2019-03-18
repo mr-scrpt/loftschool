83d487d27d1692ce3ed4cb0021543f3d88c024fd
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-18 12:21:33 +0200

    мелкие доработки

--- webpack.config.js
+++ webpack.config.js
@@ -17,31 +17,6 @@ module.exports = (env, argv) => {
 
 
 
-
-
-
-  require('postcss-mixins')({
-    mixins: {
-      icons: function (mixin, dir) {
-        fs.readdirSync('/images/' + dir).forEach(function (file) {
-          var icon = file.replace(/\.svg$/, '');
-          var rule = postcss.rule({ selector: '.icon.icon-' + icon });
-          rule.append({
-            prop:  'background',
-            value: 'url(' + dir + '/' + file + ')'
-          });
-          mixin.replaceWith(rule);
-        });
-      }
-    }
-  });
-
-
-
-
-
-
-
   const pcss = {
     test: /\.(p|post|)css$/,
     use: [
