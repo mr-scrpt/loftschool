b34473c903ee5541e9128a6b190493ee7ca7d1a7
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-24 10:57:36 +0200

    начало работы с vue, реализация скилов

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,29 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+const skill = {
+  template: "#skill"
+};
+
+const skillsRow = {
+  template: "#skills-row"
+  ,components: {
+    skill
+  }
+};
+
+new Vue({
+  el: "#skills-component"
+  ,template: "#skills-list"
+  ,components: {
+    skillsRow
+  }
+  ,data(){
+    return{
+      skills:{}
+    }
+  }
+  ,created(){
+    const data = require('../data/skills');
+    this.skills = data;
+  }
+});
