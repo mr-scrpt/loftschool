9a331ae1ef5802dd7ada8c0dc649a7a63f8a414d
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-24 15:35:27 +0200

    реализованы скилы на vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -2,12 +2,33 @@ import Vue from 'vue';
 
 const skill = {
   template: "#skill"
+  , props:{
+    skillName: String,
+    skillPercent: Number
+  }
+  ,methods:{
+    drawColoredCricle(){
+      const circle = this.$refs['circle-color'];
+      const dashArray =parseInt(
+        getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+      );
+      const percent = (dashArray / 100) * (100 - this.skillPercent);
+      circle.style.strokeDashoffset = percent;
+      console.log(dashArray)
+    }
+  }
+  , mounted() {
+    this.drawColoredCricle();
+  }
 };
 
 const skillsRow = {
   template: "#skills-row"
   ,components: {
     skill
+  }
+  ,props:{
+    skill: Object
   }
 };
 
