5fa15efd14e0384d57433745f32712ae9809f06e
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-28 16:33:05 +0200

    не отображаются блоки слайдера с комментариями

--- src/scripts/vue-comments.js
+++ src/scripts/vue-comments.js
@@ -1,45 +1,55 @@
 import Vue from 'vue';
+import Flickity from 'vue-flickity';
 
+/*
 const controls ={
   template: "#comments-controls"
 };
 
 const comments ={
-  template: "#comments-inner "
+  template: "#comments-inner"
   ,props:{
-    comments
+    comments: Array
+    ,Flickity: Object
   }
 };
+*/
 
 new Vue({
   template: '#comments-container'
   ,el: '#comments-component'
   ,components:{
-    controls
-    ,comments
+    Flickity
   }
   ,data(){
     return{
       comments: []
+      ,flickityOptions: {
+        initialIndex: 3,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true
+      }
     }
   }
-  ,props:{
-    activeComments: Array
-  }
+
   ,methods:{
-    filterCommentsShow(arr,shift,start){
-      const cloneArr = [...arr];
-      const filterArr = cloneArr.slice(start, shift);
-      return filterArr;
+    makeArrWithReqImg(data){
+      return data.map(item=>{
+          item.photo = require(`../images/content/${item.photo}`);
+          return item;
+        })
+      }
+    ,next() {
+      this.$refs.flickity.next();
     }
-    ,rollComments(dir){
-      const len = this.comments.length - 1;
-      console.log(dir,len);
+    ,previous() {
+      this.$refs.flickity.previous();
     }
   }
   ,created(){
     const data = require('../data/comments');
-    this.comments = this.filterCommentsShow(data,2, 0);
+    this.comments = this.works = this.makeArrWithReqImg(data);
 
   }
 });
