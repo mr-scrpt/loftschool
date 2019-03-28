9600a4a2fbdde9749afcca57b45ebc4e7b106c7b
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-28 11:40:27 +0200

    компонент выводит два коментария, реализованы событич по кнопкам, работа над сменой комментариев по клику

--- src/scripts/vue-comments.js
+++ src/scripts/vue-comments.js
@@ -1,6 +1,45 @@
 import Vue from 'vue';
 
+const controls ={
+  template: "#comments-controls"
+};
+
+const comments ={
+  template: "#comments-inner "
+  ,props:{
+    comments
+  }
+};
+
 new Vue({
   template: '#comments-container'
   ,el: '#comments-component'
+  ,components:{
+    controls
+    ,comments
+  }
+  ,data(){
+    return{
+      comments: []
+    }
+  }
+  ,props:{
+    activeComments: Array
+  }
+  ,methods:{
+    filterCommentsShow(arr,shift,start){
+      const cloneArr = [...arr];
+      const filterArr = cloneArr.slice(start, shift);
+      return filterArr;
+    }
+    ,rollComments(dir){
+      const len = this.comments.length - 1;
+      console.log(dir,len);
+    }
+  }
+  ,created(){
+    const data = require('../data/comments');
+    this.comments = this.filterCommentsShow(data,2, 0);
+
+  }
 });
