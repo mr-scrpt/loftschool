1f39fe5571b118cf0cc00381adb06b54e3b56e74
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-28 17:11:01 +0200

    второй слайдер

--- src/scripts/vue-comments.js
+++ src/scripts/vue-comments.js
@@ -1,34 +1,23 @@
 import Vue from 'vue';
-import Flickity from 'vue-flickity';
-
-/*
-const controls ={
-  template: "#comments-controls"
-};
-
-const comments ={
-  template: "#comments-inner"
-  ,props:{
-    comments: Array
-    ,Flickity: Object
-  }
-};
-*/
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
 
 new Vue({
   template: '#comments-container'
   ,el: '#comments-component'
   ,components:{
-    Flickity
+    swiper,
+    swiperSlide
   }
   ,data(){
     return{
       comments: []
-      ,flickityOptions: {
-        initialIndex: 3,
-        prevNextButtons: false,
-        pageDots: false,
-        wrapAround: true
+      ,swiperOption: {
+        navigation: {
+          nextEl: '.slider__next'
+          ,prevEl: '.slider__prev'
+        }
+        ,loop: true
+        ,slidesPerView: 2
       }
     }
   }
@@ -40,12 +29,6 @@ new Vue({
           return item;
         })
       }
-    ,next() {
-      this.$refs.flickity.next();
-    }
-    ,previous() {
-      this.$refs.flickity.previous();
-    }
   }
   ,created(){
     const data = require('../data/comments');
