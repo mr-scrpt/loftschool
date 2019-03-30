139207844b19513d5a77c6b1b0d6d3b0936639c1
mrScrpt <dolcegabbana2010@gmail.com>
2019-03-30 11:22:41 +0200

    начало работы над версткой админки

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,42 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  .page__inner
+    header.header
+    section.content
 </template>
+
+<style lang="postcss">
+.page{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  font: 16px / 1.42 'Open Sans', Arial, sans-serif;
+  min-width: 100%;
+  margin: 0;
+  color: $text-color;
+  background: #fff;
+}
+.page__inner{
+  min-height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 82px 1fr;
+  grid-template-areas:
+    "header"
+    "content"
+;}
+.header{
+  grid-area: header;
+  background: $violet;
+}
+.content{
+  grid-area: content;
+}
+
+
+
+
+
+
+
+
+</style>
