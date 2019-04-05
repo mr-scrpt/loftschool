e965f6591a91df0002d1125fdbf0659f5c1a4eb7
mrScrpt <dolcegabbana2010@gmail.com>
2019-04-05 20:47:23 +0300

    валидация формы

--- src/scripts/vue-form-comments.js
+++ src/scripts/vue-form-comments.js
@@ -1,47 +1,88 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 new Vue({
   template: "#comments-form-tpl"
   ,el: '#comments-form'
+  ,components:{
+    axios
+  }
   ,data(){
     return{
-      value: []
+
     }
   }
   ,props:{
-    validField: String // Строка с именами полей, которые не прошли валидацию и будут выведены в сообщении
+    name: String
+    ,email: String
+    ,message: String
+
   }
   ,mounted(){
     this.$refs.alertMessage.classList.add('page__hidden'); // пярчим блок который выводит ошибки, покзываем только в том случае если есть незаполненные поля
   }
-  ,methods:{
-    // Вспомагательный метод, для получения из формы полей для валидации, принимет форму, в которой нужно произвести валидацию
-    getFormValues(form){
-      return Array.from(form).filter(item=>{
-        if (item.tagName === "INPUT" || item.tagName === "TEXTAREA"){
-          return item;
+  ,methods: {
+    // Вспомогательная функция - Отчистка полей после
+    clearField(field){
+      field.value = "";
+    }
+    // Вспомогательная функция - Отправка запроса xhr
+    ,sendXHR(url, data, method) {
+      axios[method](url, data)
+        .then((response) => {
+          // Если ответ получен, отчищаем поля формы
+          this.clearField(this.$refs["input-name"]);
+          this.clearField(this.$refs["input-email"]);
+          this.clearField(this.$refs["textarea-message"]);
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    // Вспомогательная функция - Проверяет поля на заполненность, принимает поле
+    ,validField(field){
+      if(!field.value){
+        // Если поле пустое то добавляем ему класс для визуализации
+        field.classList.add('field-empty');
+        const messageBlock = this.$refs.alertMessage;
+        // Если поле пустое то показываем блок с ошибкой
+        if(messageBlock.classList.contains("page__hidden")){
+          messageBlock.classList.remove("page__hidden");
         }
-      });
-    }
-    // Вспомагательный метод, получает массив полей, которые были пустыми при отправке формы и выводит их в блок для отображения ошибок, так же принемает сам блок, для того, что бы убрать css класс который его скрывает
-    ,alertMessageForm(elems, block){
-      block.classList.remove('page__hidden');
-      this.validField = elems.join(',');
 
-    }
-    // Основной метод, принимает узел формы для валидации и проверяет ее и отправляет запрос на сервер при помощи вспомогательных методов
-    ,checkForm(form){
-      const elems = this.getFormValues(form);
-      let emptyValue = [];
-      elems.map(elem=>{
-        const value = elem.value;
-        if(!value){
-          emptyValue.push(elem.placeholder);
+      }else {
+        // Если поле не пустое, то проверяем есть ли у него класс пустого поля и если есть, то удаляем его
+        if(field.classList.contains('field-empty')){
+          field.classList.remove('field-empty');
         }
-      });
-      console.log(emptyValue);
-      this.alertMessageForm(emptyValue, this.$refs.alertMessage);
+      }
     }
-
+    //Основная функция валидации, вызываеться по событию клика на submit
+    ,validEmptyFields() {
+      // Получаем значения из плей в props
+      this.name = this.$refs["input-name"].value;
+      this.email = this.$refs["input-email"].value;
+      this.message = this.$refs["textarea-message"].value;
+      // Если все поля заполнены
+      if (this.name && this.email && this.message) {
+        // Фомируем тело запроса
+        const data = {
+          name: this.name
+          , email: this.email
+          , message: this.message
+        };
+        //Проверяем не был ли до этого показан блок с ошибкой формы, если да, то убираем его
+        if(!this.$refs.alertMessage.classList.contains("page__hidden")){
+          this.$refs.alertMessage.classList.add("page__hidden");
+        }
+        // Отправляем запрос
+        this.sendXHR('/testURL', data, 'post');
+      } else {
+        // Проверяем какие поля не были заполнены
+        this.validField(this.$refs["input-name"]);
+        this.validField(this.$refs["input-email"]);
+        this.validField(this.$refs["textarea-message"]);
+      }
+    }
   }
 });
